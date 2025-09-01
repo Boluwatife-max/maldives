2a13492c49f74a6d6b2381dbecf601d6bc857b57
Boluwatife-max <boluwatifebalogun57@gmail.com>
2025-09-01 18:02:26 +0100

    Add files via upload

---
+++ tour.js
@@ -0,0 +1,9 @@
+function book() {
+     alert("Your 5-star retreat is reserved. Time to pack for pure bliss");
+}
+function adv() {
+    alert("Adventure locked in! See you under the sea!");
+}
+function cruise() {
+    alert("Booking confirmed! Soon you will be sailing into the sunset.");
+}
